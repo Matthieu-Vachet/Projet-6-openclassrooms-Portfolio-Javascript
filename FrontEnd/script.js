e21ab6f6cc5efbc6a8cc2a9e3b376e17a28996b6
Matthieu-V <79032023+Matthieu-Vachet@users.noreply.github.com>
2023-08-17 14:40:43 +0200

    Add script récuperation filtre

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -23,5 +23,27 @@ function genererWorks(works) {
     worksElement.appendChild(nomElement);
   }
 }
-
 genererWorks(works);
+
+
+const reponseCategories = await fetch("http://localhost:5678/api/categories");
+const categories = await reponseCategories.json();
+
+function genererCategories(categories) {
+  for (let i = 0; i < categories.length; i++) {
+
+    const filter = categories[i];
+    // Récupération de l'élément du DOM qui accueillera les traveaux //
+    const sectionFilter = document.querySelector(".button-filtre");
+    // Création d’une balise dédiée à un travail //
+
+    // Création des balises //
+    const buttonElement = document.createElement("button");
+    buttonElement.innerText = filter.name;
+
+    // On rattache la balise article a la section Gallerie //
+    sectionFilter.appendChild(buttonElement);
+  }
+  
+}
+genererCategories(categories);
