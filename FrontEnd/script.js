6ee29d46e6c15b94bddc6ed3974405807f74030a
Matthieu-V <79032023+Matthieu-Vachet@users.noreply.github.com>
2023-08-17 15:20:44 +0200

    Add data-category au projet

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,9 +1,11 @@
+//
+// RECUPERATION DES PROJETS
+//
 const reponse = await fetch("http://localhost:5678/api/works/");
 const works = await reponse.json();
 
 function genererWorks(works) {
   for (let i = 0; i < works.length; i++) {
-
     const article = works[i];
     // Récupération de l'élément du DOM qui accueillera les traveaux //
     const sectionGallery = document.querySelector(".gallery");
@@ -17,6 +19,9 @@ function genererWorks(works) {
     const nomElement = document.createElement("figcaption");
     nomElement.innerText = article.title;
 
+    // Ajouter le nom de la catégorie comme attribut de données à worksElement
+    worksElement.setAttribute("data-category", article.category.name);
+
     // On rattache la balise article a la section Gallerie //
     sectionGallery.appendChild(worksElement);
     worksElement.appendChild(imageElement);
@@ -25,25 +30,30 @@ function genererWorks(works) {
 }
 genererWorks(works);
 
+//
+// RECUPERATION DES CATEGORIES
+//
 
 const reponseCategories = await fetch("http://localhost:5678/api/categories");
 const categories = await reponseCategories.json();
 
 function genererCategories(categories) {
   for (let i = 0; i < categories.length; i++) {
-
     const filter = categories[i];
     // Récupération de l'élément du DOM qui accueillera les traveaux //
     const sectionFilter = document.querySelector(".button-filtre");
-    // Création d’une balise dédiée à un travail //
 
-    // Création des balises //
+    // Création de la balise button //
     const buttonElement = document.createElement("button");
     buttonElement.innerText = filter.name;
 
-    // On rattache la balise article a la section Gallerie //
+    // On rattache la balise l'élément DOM a la balise Boutton //
     sectionFilter.appendChild(buttonElement);
   }
-  
 }
 genererCategories(categories);
+
+works.forEach((work) => {
+  const categoryName = work.category.name;
+  console.log(categoryName); // Affiche le nom de la catégorie pour chaque projet
+});
